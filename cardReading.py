94f594d197533eeae971048bf6131171cdbae066
kuifeng <1161634940@qq.com>
2020-05-13 09:39:14 +0800

    202005130939

--- cardReading.py
+++ cardReading.py
@@ -2,7 +2,7 @@
 # @Author: [FENG] <1161634940@qq.com>
 # @Date:   2020-05-12 12:35:58
 # @Last Modified by:   [FENG] <1161634940@qq.com>
-# @Last Modified time: 2020-05-12 13:51:58
+# @Last Modified time: 2020-05-13 09:20:41
 
 import imutils
 import cv2
@@ -10,12 +10,12 @@ import csv
 
 class cardReading(object):
 
-    def reading(self, url):
+    def reading(self, url, count):
         # pass
         xt0=[0,700,1140,1585,2030,2470,2910,3355,3790,4240,4690,5000]
         yt0=[2144,2250,2344,2440,2540,2630,2730,2830,2920,3020,3115]
         xt1=[0,570,730,880,1025,1175,1445,1600,1745,1900,2050,2330,2470,2620,2780,2930,3090,3230,3390,3540,3700,3880,4030,4180,4335,4490,5000]
-        yt1=[3500,3620,3740,3865,3975,4140,4300,4400,4530,4645,4800,4960,5080,5200,5310,5440]
+        yt1=[3450,3620,3740,3865,3975,4140,4300,4400,4530,4645,4800,4960,5080,5200,5310,5440]
         student = '' # 学号
         IDAnswer = [] # 对应题号及答案
 
@@ -35,7 +35,7 @@ class cardReading(object):
         fImage = cv2.resize(image, (5000, 7000), cv2.INTER_LANCZOS4)
 
         ChQImg = cv2.blur(thresh, (50, 50))
-        ChQImg = cv2.threshold(ChQImg, 20, 225, cv2.THRESH_BINARY)[1]
+        ChQImg = cv2.threshold(ChQImg, 60, 225, cv2.THRESH_BINARY)[1]
 
         NumImg=cv2.blur(thresh,(15,15)) # 新的二值化图
         NumImg=cv2.threshold(NumImg, 100, 255, cv2.THRESH_BINARY)[1]
@@ -63,7 +63,7 @@ class cardReading(object):
         for c in cnts:
         	# 计算轮廓的边界框，然后利用边界框数据计算宽高比
         	(x, y, w, h) = cv2.boundingRect(c)
-        	if ((y>3500 and y<5440) or (y>2110 and y<3140)) and x > 400 and w > 60 and h > 20:
+        	if ((y>3500 and y<5440) or (y>2110 and y<3140)) and x > 400 and w > 80 and h > 20:
         	    M = cv2.moments(c)
         	    cX = int(M["m10"] / M["m00"])
         	    cY = int(M["m01"] / M["m00"])
@@ -73,6 +73,10 @@ class cardReading(object):
         	    #保存题目坐标信息
         	    Answer.append((cX, cY))
 
+
+        next_link = "D:/Python/www/duka/%02d.jpg" % count;
+        cv2.imwrite(next_link,fImage)
+
         for i in Answer:
             if i[1] > yt0[0] and i[1] < yt0[-1]:
                 for j in range(0,len(xt0)-1):
@@ -81,21 +85,22 @@ class cardReading(object):
                             if i[1]>yt0[k] and i[1]<yt0[k+1]:
                                 student = str(k) + student
             else :
-                for j in range(0,len(xt1)-1):
-                    if i[0]>xt1[j] and i[0]<xt1[j+1]:
-                        for k in range(0,len(yt1)-1):
-                            if i[1]>yt1[k] and i[1]<yt1[k+1]:
-                                option = self.judge0(j, k, i[0], i[1])
-                                IDAnswer.append(option)
+                pass
+                # for j in range(0,len(xt1)-1):
+                #     if i[0]>xt1[j] and i[0]<xt1[j+1]:
+                #         for k in range(0,len(yt1)-1):
+                #             if i[1]>yt1[k] and i[1]<yt1[k+1]:
+                #                 option = self.judge0(j, k, i[0], i[1])
+                #                 IDAnswer.append(option)
 
         IDAnswer.sort()
         newIDAnswer = {'学号':student};
-        for x in IDAnswer:
-            if x[0] <= 60:
-                if x[0] not in newIDAnswer:
-                    newIDAnswer[x[0]] = x[1]
-                else :
-                    newIDAnswer[x[0]] = newIDAnswer[x[0]] + x[1]
+        # for x in IDAnswer:
+        #     if x[0] <= 60:
+        #         if x[0] not in newIDAnswer:
+        #             newIDAnswer[x[0]] = x[1]
+        #         else :
+        #             newIDAnswer[x[0]] = newIDAnswer[x[0]] + x[1]
 
         return newIDAnswer;
 
@@ -145,7 +150,11 @@ class cardReading(object):
 
     
 if __name__=="__main__":
-    url = 'D:/Python/www/duka/image/demo.jpg'
+    url = ['D:/Python/www/duka/image/demo.jpg', 'D:/Python/www/duka/image/1.jpg', 'D:/Python/www/duka/image/2.jpg', 'D:/Python/www/duka/image/3.jpg']
     cardReading = cardReading()
-    aaaa = cardReading.reading(url)
-    print(aaaa)
+
+    for x in range(0,len(url)):
+        # print(url[x])
+        aaaa = cardReading.reading(url[x], x)
+        print(aaaa)
+
